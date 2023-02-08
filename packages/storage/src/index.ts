6c45ec0daf18ad83b9fcb69c054b53589b335c77
shomix <shomixpetrovic@gmail.com>
2023-02-09 00:43:35 +0100

    Add upload manager logic

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+import UploadManager from "./upload-manager";
+import { ProtocolEnum } from "./enums";
+
+export { ProtocolEnum };
+
+export interface SpheronClientConfiguration {
+  token: string;
+}
+
+export default class SpheronClient {
+  private readonly configuration: SpheronClientConfiguration;
+
+  constructor(configuration: SpheronClientConfiguration) {
+    this.configuration = configuration;
+  }
+
+  async upload(
+    path: string,
+    configuration: { name: string; protocol: ProtocolEnum }
+  ) {
+    const uploadManager = new UploadManager(this.configuration);
+    await uploadManager.upload({
+      path: path,
+      name: configuration.name,
+      protocol: configuration.protocol,
+    });
+  }
+}
---
+++ packages/storage/src/upload-manager/index.ts
@@ -0,0 +1,136 @@
+import axios, { AxiosRequestConfig } from "axios";
+import FormData from "form-data";
+import pLimit from "p-limit";
+import { ProtocolEnum } from "../enums";
+import PayloadCreator from "./upload-context";
+
+export interface UploadMangerConfiguration {
+  token: string;
+}
+
+export interface UploadConfiguration {
+  path: string;
+  protocol: ProtocolEnum;
+  name: string;
+}
+
+class UploadManager {
+  // private readonly uploadApiUrl = "https://api-dev.spheron.network";
+  private readonly uploadApiUrl: string = "http://localhost:8002";
+
+  private readonly MAX_PAYLOAD_SIZE = 1024 * 1024 * 6;
+  private readonly PARALLEL_UPLOAD_COUNT = 5;
+  private readonly configuration: UploadMangerConfiguration;
+
+  constructor(configuration: UploadMangerConfiguration) {
+    this.configuration = configuration;
+  }
+
+  public async upload(configuration: UploadConfiguration): Promise<void> {
+    this.validateUploadConfiguration(configuration);
+
+    const payloadCreator = new PayloadCreator(
+      configuration.path,
+      this.MAX_PAYLOAD_SIZE
+    );
+    const payloads = await payloadCreator.createPayloads();
+
+    const { deploymentId } = await this.startDeployment(
+      configuration.protocol,
+      configuration.name
+    );
+    await this.uploadPayloads(deploymentId, payloads);
+    await this.finalizeUploadDeployment(deploymentId);
+  }
+
+  private async startDeployment(
+    protocol: string,
+    projectName: string
+  ): Promise<{ deploymentId: string }> {
+    try {
+      const response = await axios.post<{ deploymentId: string }>(
+        `${this.uploadApiUrl}/v1/upload-deployment?protocol=${protocol}&project=${projectName}`,
+        {},
+        this.getAxiosRequestConfig()
+      );
+      return response.data;
+    } catch (error) {
+      const errorMessage = error?.data?.message || error?.message;
+      throw new Error(errorMessage);
+    }
+  }
+
+  private async uploadPayloads(
+    deploymentId: string,
+    payloads: FormData[]
+  ): Promise<void> {
+    const limit = pLimit(this.PARALLEL_UPLOAD_COUNT);
+
+    await Promise.all(
+      payloads.map((payload, index) =>
+        limit(() => this.uploadPayload(payload, deploymentId, index))
+      )
+    );
+  }
+
+  private async uploadPayload(
+    payload: FormData,
+    deploymentId: string,
+    payloadIndex: number
+  ): Promise<void> {
+    try {
+      await axios.post(
+        `${this.uploadApiUrl}/v1/upload-deployment/${deploymentId}/data`,
+        payload,
+        this.getAxiosRequestConfig()
+      );
+    } catch (error) {
+      console.log(`Error for chunk ${payloadIndex} - ${error.message}`);
+    }
+  }
+
+  private async finalizeUploadDeployment(deploymentId: string): Promise<void> {
+    try {
+      await axios.post<{
+        deploymentId: string;
+        affectedDomains: string;
+      }>(
+        `${this.uploadApiUrl}/v1/upload-deployment/${deploymentId}/finish?action=UPLOAD`,
+        {},
+        this.getAxiosRequestConfig()
+      );
+    } catch (error) {
+      const errorMessage = error?.data?.message || error?.message;
+      throw new Error(errorMessage);
+    }
+  }
+
+  private validateUploadConfiguration(
+    configuration: UploadConfiguration
+  ): void {
+    const supportedProtocols = Object.values(ProtocolEnum);
+    if (supportedProtocols.indexOf(configuration.protocol) === -1) {
+      throw new Error(
+        `Protocol '${
+          configuration.protocol
+        }' is not supported. Supported protocols are [${supportedProtocols.join(
+          ", "
+        )}].`
+      );
+    }
+
+    if (!configuration.name) {
+      throw new Error("Name is not provided.");
+    }
+  }
+
+  private getAxiosRequestConfig(): AxiosRequestConfig {
+    return {
+      headers: {
+        Authorization: `Bearer ${this.configuration.token}`,
+      },
+    };
+  }
+}
+
+export default UploadManager;
