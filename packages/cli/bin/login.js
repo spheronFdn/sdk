df3ba21ba260a3c20bf5f4dc9a066fddd6036e77
dusanstanis <58050163+dusanstanis@users.noreply.github.com>
2023-02-13 10:57:59 +0100

    [Dusan/sph 1003] - login (#5)
    * Login with local server expecting redirect from github
    
    * remove async-listen npm dep
    
    * Added writing of jwt token in spheron config file. Added status log after loging process
    
    * Updated logic so bitbucket login can work

--- packages/cli/bin/login.js
+++ packages/cli/bin/login.js
@@ -1,5 +1,67 @@
-async function login() {
+const http = require("http");
+const open = require("open");
+const axios = require("axios");
+const { writeToConfigFile } = require("./utils");
+
+async function login(provider) {
   console.log("Login...");
+  const server = http.createServer();
+  const s = await server.listen(0, "127.0.0.1"); // 0 = random port
+  const { port } = s.address();
+  const baseURL = `http://localhost:8080/auth/${provider}/cli/login`;
+  const fullURL = baseURL + `?port=${port}`;
+  console.log(fullURL);
+  const successLocationRedirect = new URL(
+    "https://app.spheron.network/notifications/cli-login"
+  );
+  let loginError = false;
+  try {
+    await Promise.all([
+      new Promise((resolve, reject) => {
+        server.once("request", async (req, res) => {
+          console.log(req.url);
+          const code = req.url.split("&")[0].split("=")[1];
+          console.log(code);
+
+          const verify = await axios.get(
+            `http://localhost:8080/auth/${provider}/cli/verify-token/${code}?port=${port}`, //port used for bitbucket
+            {
+              headers: {
+                Accept: "application/json",
+              },
+            }
+          );
+          if (verify.status != 200 || !verify.data.jwtToken) {
+            loginError = true;
+            throw new Error("Verification of token failed");
+          }
+
+          const jwt = verify.data.jwtToken;
+          // Closing of server
+          res.setHeader("connection", "close");
+          res.statusCode = 302;
+          res.setHeader("location", successLocationRedirect.href);
+          res.end();
+          //store jwt token in spheron-config file
+          await writeToConfigFile("jwtToken", jwt);
+          resolve();
+        });
+        server.once("error", reject);
+      }),
+      open(fullURL),
+    ]);
+  } catch (error) {
+    // console.log("Error: ", error.text);
+    loginError = true;
+  } finally {
+    server.close();
+    if (loginError) {
+      console.log("Error occured while loging in, ");
+    } else {
+      console.log("Login succesfull!");
+    }
+    process.exit(0);
+  }
 }
 
 module.exports = {
