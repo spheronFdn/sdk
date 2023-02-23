6914f7418c807afb43ab3b061508c7a0de1384f0
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2023-02-23 12:27:10 +0100

    Fix for publishing only dist folder

--- packages/cli/build/scripts/postinstall.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const create_configuration_1 = require("../create-configuration");
-(0, create_configuration_1.createConfiguration)();
