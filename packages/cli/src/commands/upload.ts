1fb5f3717353fae8f997d360688eb396213411b2
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2023-02-28 17:00:21 +0100

    Add progress bar when doing upload

--- packages/cli/src/commands/upload.ts
+++ packages/cli/src/commands/upload.ts
@@ -1,5 +1,6 @@
 import configuration from "../configuration";
 const { default: SpheronClient, ProtocolEnum } = require("@spheron/storage");
+import cliProgress from "cli-progress";
 
 import { readFromJsonFile } from "../utils";
 
@@ -21,6 +22,14 @@ export async function upload(
     const client = new SpheronClient({ token: jwtToken });
 
     let uploadedBytes = 0;
+
+    // Initialize the progress bar
+    const progressBar = new cliProgress.SingleBar({
+      format:
+        "Uploading [{bar}] {percentage}% | ETA: {eta_formatted} | {value}/{total} bytes",
+      stopOnComplete: true,
+    });
+    let uploadStarted = false;
     const { uploadId, bucketId, protocolLink, dynamicLinks } =
       await client.upload(rootPath, {
         protocol: mapProtocol(protocol),
@@ -30,10 +39,12 @@ export async function upload(
           console.log("Upload started, id of deployment: " + deploymentId);
         },
         onChunkUploaded(uploadedSize: any, totalSize: any) {
+          if (!uploadStarted) {
+            uploadStarted = true;
+            progressBar.start(totalSize, 0);
+          }
           uploadedBytes += uploadedSize;
-          console.log(
-            `Uploaded ${uploadedBytes} bytes of total ${totalSize} bytes`
-          );
+          progressBar.update(uploadedBytes);
         },
       });
     console.log("uploadId:", uploadId);
