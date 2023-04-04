9e3256b24c41f37889c01e59a292d099dd2baa4e
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2023-04-04 11:42:52 +0200

    Revert change of hosting-api address in configuration

--- packages/cli/src/configuration.ts
+++ packages/cli/src/configuration.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 const configuration = {
-  spheronServerAddress: "http://localhost:8080",
+  spheronServerAddress: "https://api-v2.spheron.network",
   spheronFrontendAddress: "https://app.spheron.network",
   version: "1.0.4",
   configFilePath: path.join(
