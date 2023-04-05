48a418840d642ce001b44ea310e9308d1ad6cc66
dusanstanis <dusan.stanisavljevicposlovni98@gmail.com>
2023-04-05 12:35:02 +0200

    Fix import of core lib in index.ts

--- packages/cli/src/configuration.ts
+++ packages/cli/src/configuration.ts
@@ -3,7 +3,7 @@ import path from "path";
 const configuration = {
   spheronServerAddress: "https://api-v2.spheron.network",
   spheronFrontendAddress: "https://app.spheron.network",
-  version: "1.0.6",
+  version: "1.0.7",
   configFilePath: path.join(
     process.env.HOME ? process.env.HOME : "/home/ubuntu",
     ".spheron/config.json"
