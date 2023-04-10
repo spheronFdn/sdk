7dd6a02b7a42b1b232909fea254c3a0918b3b7a1
Manank Patni <manank321@gmail.com>
2023-04-10 17:17:58 +0530

    Return IIPNS name from client
    Signed-off-by: Manank Patni <manank321@gmail.com>

--- packages/storage/src/index.ts
+++ packages/storage/src/index.ts
@@ -10,6 +10,7 @@ import BucketManager, {
   UploadStatusEnum,
 } from "./bucket-manager";
 import {
+  IIPNSName,
   IPNSPublishResponse,
   TokenScope,
   UsageWithLimits,
@@ -139,30 +140,28 @@ export class SpheronClient {
     await this.bucketManager.unarchiveBucket(bucketId);
   }
 
-  async publishIPNS(uploadId: string): Promise<IPNSPublishResponse> {
+  async publishIPNS(uploadId: string): Promise<IIPNSName> {
     return await this.spheronApi.publishIPNS(uploadId);
   }
 
   async updateIPNSName(
     ipnsNameId: string,
     uploadId: string
-  ): Promise<IPNSPublishResponse> {
+  ): Promise<IIPNSName> {
     return await this.spheronApi.updateIPNSName(ipnsNameId, uploadId);
   }
 
-  async getIPNSName(ipnsNameId: string): Promise<IPNSPublishResponse> {
+  async getIPNSName(ipnsNameId: string): Promise<IIPNSName> {
     return await this.spheronApi.getIPNSName(ipnsNameId);
   }
 
-  async getIPNSNamesForDeployment(
-    deploymentId: string
-  ): Promise<IPNSPublishResponse[]> {
+  async getIPNSNamesForDeployment(deploymentId: string): Promise<IIPNSName[]> {
     return await this.spheronApi.getIPNSNamesForDeployment(deploymentId);
   }
 
   async getIPNSNamesForOrganization(
     organizationId: string
-  ): Promise<IPNSPublishResponse[]> {
+  ): Promise<IIPNSName[]> {
     return await this.spheronApi.getIPNSNamesForOrganization(organizationId);
   }
 
@@ -204,5 +203,36 @@ export class SpheronClient {
     return await this.spheronApi.getTokenScope();
   }
 }
+
+(async () => {
+  const spheronClient = new SpheronClient({
+    token:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlLZXkiOiJjMTQ2ZmVjY2E1NGNkNjU2YWJkZjViMzY5ZDc2MDUxY2MwMTg2OGZkMmY4YjIzMjIwNjhiYjNmYzZmZWM1Nzc3NWI0ZjFmZDM3ZmY1MDliYjY3ZTcwOTA5ODA0NmJjYTU3OGY5NjkyN2I2ZmNkOTFmYTFlNjZkYjdkZjM1NGQ5MyIsImlhdCI6MTY4MDg2OTkzNiwiaXNzIjoid3d3LnNwaGVyb24ubmV0d29yayJ9.DHbZWb9oCel05IcGcS90XPradGnLV0jC_KnBbGbZOlE",
+  });
+
+  const resp = await spheronClient.publishIPNS("642d7526dad7300012d3fb7c");
+  console.log("resp: ", resp);
+
+  const allipns = await spheronClient.getIPNSNamesForOrganization(
+    "642d7331dad7300012d3fb02"
+  );
+  console.log("allipns: ", allipns);
+
+  const allipnsdeployment = await spheronClient.getIPNSNamesForDeployment(
+    "642d7526dad7300012d3fb7c"
+  );
+  console.log("allipnsdeployment: ", allipnsdeployment);
+
+  const allipnsName = await spheronClient.getIPNSName(
+    "6433a64908a7d758d04b9e10"
+  );
+  console.log("allipnsName: ", allipnsName);
+
+  const updateIPNS = await spheronClient.updateIPNSName(
+    "6433a64908a7d758d04b9e10",
+    "642d7526dad7300012d3fb7c"
+  );
+  console.log("updateIPNS: ", updateIPNS);
+})();
 
 export default SpheronClient;
