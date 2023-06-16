e2f220e8a88eafcd31d25d67fde91d93668af06a
shomix <100691266+shomix@users.noreply.github.com>
2023-06-16 17:58:34 +0200

    Add support for onUploadInitiated & onChunkUploaded to encryption storage sdk (#130)

--- packages/browser-upload/src/interface.ts
+++ packages/browser-upload/src/interface.ts
@@ -15,13 +15,13 @@ export interface EncryptToIpfsProps {
   // the session signatures to use to authorize the user with the nodes
   sessionSigs?: any;
 
-  // The access control conditions that the user must meet to obtain this signed token.  This could be posession of an NFT, for example.  You must pass either accessControlConditions or evmContractConditions or solRpcConditions or unifiedAccessControlConditions.
+  // The access control conditions that the user must meet to obtain this signed token.  This could be possession of an NFT, for example.  You must pass either accessControlConditions or evmContractConditions or solRpcConditions or unifiedAccessControlConditions.
   accessControlConditions?: AccessControlConditions;
 
-  // EVM Smart Contract access control conditions that the user must meet to obtain this signed token.  This could be posession of an NFT, for example.  This is different than accessControlConditions because accessControlConditions only supports a limited number of contract calls.  evmContractConditions supports any contract call.  You must pass either accessControlConditions or evmContractConditions or solRpcConditions or unifiedAccessControlConditions.
+  // EVM Smart Contract access control conditions that the user must meet to obtain this signed token.  This could be possession of an NFT, for example.  This is different than accessControlConditions because accessControlConditions only supports a limited number of contract calls.  evmContractConditions supports any contract call.  You must pass either accessControlConditions or evmContractConditions or solRpcConditions or unifiedAccessControlConditions.
   evmContractConditions?: EvmContractConditions;
 
-  // Solana RPC call conditions that the user must meet to obtain this signed token.  This could be posession of an NFT, for example.
+  // Solana RPC call conditions that the user must meet to obtain this signed token.  This could be possession of an NFT, for example.
   solRpcConditions?: SolRpcConditions;
 
   // An array of unified access control conditions.  You may use AccessControlCondition, EVMContractCondition, or SolRpcCondition objects in this array, but make sure you add a conditionType for each one.  You must pass either accessControlConditions or evmContractConditions or solRpcConditions or unifiedAccessControlConditions.
