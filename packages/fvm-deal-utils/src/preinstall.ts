69b5f98c5e21b7f40b4badd3e552cab4785a1f5a
shomix <shomixpetrovic@gmail.com>
2023-08-10 11:15:22 +0200

    Add tag to the go binary, add logic to delete the binary before downloading it

--- packages/fvm-deal-utils/src/preinstall.ts
+++ packages/fvm-deal-utils/src/preinstall.ts
@@ -15,31 +15,33 @@ function checkDirectoryExists(directoryPath: string): boolean {
   }
 }
 
-if (!checkDirectoryExists(goCodePath)) {
-  // Download the Go code from GitHub
-  download(
-    "github:tech-greedy/generate-car",
-    goCodePath,
-    (err: Error | null) => {
-      if (err) {
-        console.error("Error downloading from github:", err);
-        process.exit(1);
-      }
-
-      // Compile the Go code into a binary
-      const goBuild = spawnSync("make", ["build"], { cwd: goCodePath });
-
-      if (goBuild.error) {
-        console.error("Failed to compile Go code:", goBuild.error.message);
-        process.exit(1);
-      }
-
-      if (goBuild.status !== 0) {
-        console.error("Failed to compile Go code. Exit code:", goBuild.status);
-        process.exit(1);
-      }
-
-      console.log("Go code downloaded and compiled successfully!");
-    }
-  );
+if (checkDirectoryExists(goCodePath)) {
+  fs.rmSync(goCodePath, { recursive: true, force: true });
 }
+
+// Download the Go code from GitHub
+download(
+  "github:tech-greedy/generate-car#v4.0.1",
+  goCodePath,
+  (err: Error | null) => {
+    if (err) {
+      console.error("Error downloading from github:", err);
+      process.exit(1);
+    }
+
+    // Compile the Go code into a binary
+    const goBuild = spawnSync("make", ["build"], { cwd: goCodePath });
+
+    if (goBuild.error) {
+      console.error("Failed to compile Go code:", goBuild.error.message);
+      process.exit(1);
+    }
+
+    if (goBuild.status !== 0) {
+      console.error("Failed to compile Go code. Exit code:", goBuild.status);
+      process.exit(1);
+    }
+
+    console.log("Go code downloaded and compiled successfully!");
+  }
+);
